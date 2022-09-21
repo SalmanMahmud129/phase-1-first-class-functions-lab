11e7d84aabe3b2f1ebbe1b5a15cbaa60af441162
Salman Mahmud <salman_mahmud@hotmail.com>
2022-09-21 10:24:41 -0400

    commit tree cf29d4e74df91304286ef3ea0ac471b2ad720551

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2)
+}
+
+// console.log(returnLastTwoDrivers(drivers))
+// console.log(drivers[drivers.length])
+// console.log(drivers[drivers.length-1])
+// console.log(drivers[drivers.length-2])
+// console.log(returnLastTwoDrivers(drivers))
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+// console.log(selectingDrivers[1](drivers))
+// console.log(selectingDrivers[0](drivers))
+
+function createFareMultiplier(num){
+    return function(fare){
+        return num * fare //this is the fare
+    }
+}
+
+const fareDoubler = (createFareMultiplier) => createFareMultiplier * 2 //the return value from the multiplier = the fare. 
+//that gets passed into the argument and it gets doubled
+
+const fareTripler = (createFareMultiplier) => createFareMultiplier * 3 
+
+const selectDifferentDrivers = (names, whichDrivers) => {
+    if(whichDrivers == returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(names)
+    }
+    else if(whichDrivers == returnLastTwoDrivers){
+        return returnLastTwoDrivers(names)
+    }
+}
+
+console.log(selectDifferentDrivers(drivers,returnFirstTwoDrivers))
+
